d5dd44869fde71f69c710ea0dfeb25d8c4c0e9f6
Joanna <aska3001@wp.pl>
2021-11-05 14:39:29 +0100

    Add project files.

---
+++ DinnerParty.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerParty
+{
+    public class DinnerParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+
+        int NumberOfPeople;
+        decimal CostOfBeveragesPerPerson;
+        decimal CostOfDecorations;
+
+        void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = NumberOfPeople * 5.0M;
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = NumberOfPeople * 20.0M;
+            }
+        }
+        void CalculateCosOfDecrations(bool decorations)
+        {
+            if (decorations)
+            {
+                CostOfDecorations = NumberOfPeople * 15.0M + 50.0M;
+            }
+            else
+            {
+                CostOfDecorations = NumberOfPeople * 7.5M + 30.0M;
+            }
+        }
+        decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = (CostOfFoodPerPerson + CostOfBeveragesPerPerson + CostOfDecorations) * NumberOfPeople;
+            if (healthyOption)
+            {
+               return totalCost = totalCost * 0.95M;
+            }
+            else
+            {
+              return  totalCost;
+            }
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinnerParty
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+      
+        public Form1()
+        {
+            InitializeComponent();
+            dinnerParty = new DinnerParty() { NumberOfPeople = 5 };
+            dinnerParty.SetHealthyOption(false);
+            dinnerParty.CalculateCostOfDecorations(true);
+            DisplayDinnerPartyCost();
+        }
+
+        private void DispayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(healthyBox.Checked);
+            costLabel.Text = Cost.ToString("c");
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.NumberOfPeople = (int)numericUpDown1.Value;
+            DisplayDinnerPartyCost();
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations();
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetHealthyOption();
+            DisplayDinnerPartyCost();
+        }
+    }
+}
